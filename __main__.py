4ae6caed405b47eaa5de90f5fa48a81dbcd5340f
jean-ws <jeanwilsonsilva0000@gmail.com>
2023-02-11 17:25:17 -0300

    remove todo unnecessary on main

--- __main__.py
+++ __main__.py
@@ -17,7 +17,6 @@ while status:
     frame.drawFrame()
     if WConio2.kbhit():
         frame.input(WConio2.getkey())
-    #TODO if de acordo com a velocidade do vilao (quanto maior velocidade, mais vezes deve entrar no IF) que executa o frame.enemy1.move
 
     count += 1 #ao final do While
 
