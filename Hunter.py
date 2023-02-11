fb75bbc43cbacb82cb56075b05449a8fb1f12566
jean-ws <jeanwilsonsilva0000@gmail.com>
2023-02-11 16:01:44 -0300

    rename enemy to hunter

---
+++ Hunter.py
@@ -0,0 +1,42 @@
+from Character import Character
+
+class Hunter(Character):
+    #atributos que somente o inimigo normal terá
+
+    def __init__(self):
+        self.icon = ["##","##"] #lista[linha de cima, linha de baixo]
+        self.position = [2,10] #lista[x,y]
+        self.speed = 3 
+        self.acceptedMoves = {
+            'x-': self._moveLeft,
+            'x+': self._moveRight,
+            'y-': self._moveUp,
+            'y+': self._moveDown,
+            }
+
+        self.hitbox = {
+            'a' : self.position,
+            'b' : [self.position[0] + 1, self.position[1]],
+            'c' : [self.position[0], self.position[1] + 1],
+            'd' : [self.position[0] + 1,self.position[1] + 1]
+        }
+    
+    def _attHitbox(self):
+        self.hitbox = {
+            'a' : self.position,
+            'b' : [self.position[0] + 1, self.position[1]],
+            'c' : [self.position[0], self.position[1] + 1],
+            'd' : [self.position[0] + 1,self.position[1] + 1]
+        }
+
+    def trackHero():
+        #TODO trackHero
+        pass
+
+    def move(self,hero,frame_width, frame_height):
+        movement = 'x+' #TODO encaixar com o metodo trackHero
+        action = self.acceptedMoves[movement]
+        action(frame_width, frame_height)
+        self._attHitbox()
+        
+    #TODO metodo que segue o heroi (usar velocidade)
