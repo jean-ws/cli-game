d2eb99182ccac8070f33ed61ea0c47ec988d03a0
Mo0inha <arthur.moura.augusto@gmail.com>
2023-02-12 20:53:12 -0300

    GameOver message print

--- GameOver.py
+++ GameOver.py
@@ -1,6 +1,8 @@
 import WConio2
 import cursor
 
+
+
 class GameOver:
     def __init__(self):
         self.frameWidth = 100
@@ -28,14 +30,29 @@ class GameOver:
         WConio2.textcolor(self.vermelho)
         print('██')
 
-    def _drawGameOver(self,n_current_line):
+    def _drawGameOver(self):
         
         WConio2.textcolor(self.vermelho)
-        print('██', end = '')
+        #print('██', end = '')
         
         WConio2.textcolor(self.roxo)
-        print('██' * 5 + '  ' * 7 + '██' * 2)
+        #print("aaa")
+        #print("aaa")
+        print(' ' * 2 + '██' * 2  + ' ' * 6  + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4)
+        print('██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 1)
+        print('██' * 1 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 4 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 3)
+        print('██' * 1 + ' '* 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 1)
+        print(' ' * 2 + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4)
+        print()
+        print(' ' * 2 + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4 + ' ' * 2 + '██' * 3)
+        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 4 + '██' * 1)
+        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 3 + ' ' * 4 + '██' * 3)
+        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 2 + '██' * 1)
+        print(' ' * 2 + '██' * 2 + ' ' * 8 + '██' * 1 + ' ' * 6 + '██' * 4 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1)
+
         #TODO desenho do game over
+        #print(' ' * 2 + '██' * 2 + ' ' * 6 + '██' * 2)
+
         
         WConio2.textcolor(self.vermelho)
         print('██')
@@ -59,9 +76,14 @@ class GameOver:
                 self._drawLine(n_current_line)
 
 
+
     def drawScreen(self):
         WConio2.gotoxy(0,0)
         self._borderUpDown()
         self._drawLines()
         self._borderUpDown()
+
+gaover = GameOver()
+gaover._drawGameOver()
+
     
