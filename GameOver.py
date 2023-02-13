7c3adedf29624bc2227897be37184cdee825d325
Mo0inha <arthur.moura.augusto@gmail.com>
2023-02-12 23:19:45 -0300

    GameOver fix and score print

--- GameOver.py
+++ GameOver.py
@@ -5,8 +5,8 @@ import cursor
 
 class GameOver:
     def __init__(self):
-        self.frameWidth = 100
-        self.frameHeight = 40
+        self.frameWidth = 62
+        self.frameHeight = 30
         self.char_background = ' '
         WConio2.clrscr()
         cursor.hide() 
@@ -23,6 +23,10 @@ class GameOver:
         WConio2.textcolor(self.vermelho)
         print('██' + '  ' * number_of_spaces, end = '')
 
+    def _drawRightBorder(self,number_of_spaces):
+        WConio2.textcolor(self.vermelho)
+        print('  ' * number_of_spaces + '██')
+
     def _drawBlankLine(self,n_current_line):
         
         WConio2.textcolor(self.vermelho)
@@ -35,49 +39,101 @@ class GameOver:
 
     def _drawGameOver(self):
         
-        self._drawLeftBorder(5)
-        
-        WConio2.textcolor(self.roxo)
-        print(' ' * 2 + '██' * 2  + ' ' * 6  + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 1)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' ' * 2 + '██' * 2 + ' ' * 2 + '██' * 4 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 2 + '██' * 3)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' '* 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 1)
-        self._drawLeftBorder(5)
-        print(' ' * 2 + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4)
-        self._drawLeftBorder(5)
-        print()
-        self._drawLeftBorder(5)
-        print(' ' * 2 + '██' * 2 + ' ' * 4 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 4 + ' ' * 2 + '██' * 3)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 6 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 4 + '██' * 1)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 3 + ' ' * 4 + '██' * 3)
-        self._drawLeftBorder(5)
-        print('██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1 + ' ' * 8 + '██' * 1 + ' ' * 2 + '██' * 1)
-        self._drawLeftBorder(5)
-        print(' ' * 2 + '██' * 2 + ' ' * 8 + '██' * 1 + ' ' * 6 + '██' * 4 + ' ' * 2 + '██' * 1 + ' ' * 4 + '██' * 1)
 
-        #TODO desenho do game over
-        #print(' ' * 2 + '██' * 2 + ' ' * 6 + '██' * 2)
-
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
         
-        WConio2.textcolor(self.vermelho)
-        print('██')
+        self._drawLeftBorder(5) 
+        print(' ' * 2 + '██' * 2  + '  ' * 3  + '██' * 2 + '  ' * 2 + '██' * 1 + '  ' * 3 + '██' * 1 + '  ' * 1 + '██' * 4, end = '') 
+        self._drawRightBorder(5)
+
+        self._drawLeftBorder(5) 
+        print('██' * 1 + ' ' * 8 + '██' * 1 + '  ' * 2 + '██' * 1 + ' ' * 2 + '██' * 2 + '  ' * 1 + '██' * 2 + '  ' * 1 + '██' * 1, end = '')
+        self._drawRightBorder(8)
+
+        self._drawLeftBorder(5)
+        print('██' * 1 + ' ' * 2 + '██' * 2 + '  ' * 1 + '██' * 4 + ' ' * 2 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 1 + '██' * 3, end = '')
+        self._drawRightBorder(6)
+        
+        self._drawLeftBorder(5)
+        print('██' * 1 + ' '* 4 + '██' * 1 + '  ' * 1 + '██' * 1 + ' ' * 4 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 3 + '██' * 1 + '  ' * 1 + '██' * 1, end = '')
+        self._drawRightBorder(8)
+
+        self._drawLeftBorder(5)
+        print(' ' * 2 + '██' * 2 + '  ' * 2 + '██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 3 + '██' * 1 + '  ' * 1 + '██' * 4, end = '')
+        self._drawRightBorder(5)
+
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+
+
+        self._drawLeftBorder(5)
+        print(' ' * 2 + '██' * 2 + '  ' * 2 + '██' * 1 + '  ' * 3 + '██' * 1 + '  ' * 1 + '██' * 4 + '  ' * 1 + '██' * 3, end = '')
+        self._drawRightBorder(6)
+
+        self._drawLeftBorder(5)
+        print('██' * 1 + ' ' * 4 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 3 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 4 + '██' * 1 + '  ' * 2 + '██' * 1, end = '')
+        self._drawRightBorder(5)
+
+        self._drawLeftBorder(5)
+        print('██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 2 + '██' * 3 + '  ' * 2 + '██' * 3, end = '')
+        self._drawRightBorder(6)
+
+        self._drawLeftBorder(5)
+        print('██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 1 + '██' * 1 + '  ' * 2 + '██' * 1 + '  ' * 4 + '██' * 1 + '  ' * 1 + '██' * 1, end = '')
+        self._drawRightBorder(6)
+
+        self._drawLeftBorder(5)
+        print('  ' * 1 + '██' * 2 + '  ' * 4 + '██' * 1 + '  ' * 3 + '██' * 4 + '  ' * 1 + '██' * 1 + '  ' * 2 + '██' * 1, end = '')
+        self._drawRightBorder(5)
+
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+        
+
     
-    def _drawScore():
-        #TODO
+    def _drawScore(self):
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+
+        self._drawLeftBorder(0)
+        print(' '* 14 + "Score: ", end = '')
+        self._drawRightBorder(7)
+
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+
         pass
 
     def _drawRecord():
         #TODO
         pass
 
-    def _drawMenu():
+    def _drawMenu(self):
         #TODO
-        pass
+        
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
+
+        self._drawLeftBorder(0)
+        print(' '* 18 + "Press [SPACE] to continue ", end = '')
+        self._drawRightBorder(8)
+
+        self._drawLeftBorder(0)
+        print('', end = '')
+        self._drawRightBorder(30)
 
     def _drawBlankLines(self):
         #TODO
@@ -90,10 +146,11 @@ class GameOver:
     def drawScreen(self):
         WConio2.gotoxy(0,0)
         self._borderUpDown()
-        self._drawLines()
+        self._drawGameOver()
+        self._drawScore()
+        self._drawMenu()
         self._borderUpDown()
+        
 
-gaover = GameOver()
-gaover._drawGameOver()
-
-    
+gover = GameOver()
+gover.drawScreen()
